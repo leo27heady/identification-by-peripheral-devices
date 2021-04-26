4f286acb0d4b1fbe92c9d36ceb6b74907bdc058a
Leonid Kulyk <leonidkulik2707@gmail.com>
2021-04-26 23:26:48 +0300

    created peripherial monitor

---
+++ monitor.py
@@ -0,0 +1,26 @@
+import time
+import datetime
+import pickle
+
+import mouse
+import keyboard
+
+
+def timeStamp(fmt='%Y-%m-%d_%H-%M-%S'):
+    return datetime.datetime.now().strftime(fmt)
+
+
+while True:
+    mouse_events = []
+    mouse.hook(mouse_events.append)
+    keyboard.start_recording()
+
+    time.sleep(10*1)
+
+    mouse.unhook(mouse_events.append)
+    keyboard_events = keyboard.stop_recording()
+
+    with open(f'dump\{timeStamp()}.pickle', 'wb') as f:
+        pickle.dump((mouse_events, keyboard_events), f)
+    
+    break
